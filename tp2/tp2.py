609bbecc400321940b2feca15ea961433b364cc6
Matias Francisco Deo <matiasfd.deo@gmail.com>
2024-09-01 15:52:17 -0300

    terminar tp2

--- tp2/tp2.py
+++ tp2/tp2.py
@@ -1,14 +1,34 @@
+
 from abc import ABC
 from datetime import date
 
 
+class Empleado(ABC):
+    def __init__(self, nombre: str, dirección: str, estadoCivil: str, fechaDeNacimiento: date, sueldoBásico: float):
+        self.nombre: str = nombre
+        self.dirección: str = dirección
+        self.estadoCivil: str = estadoCivil
+        self.fechaDeNacimiento: date = fechaDeNacimiento
+        self.sueldoBásico: float = sueldoBásico
+        
+    def calcularEdad(self)->int:
+        return self.fechaDeNacimiento.year - date.today().year
+    
+    def calcularSueldoNeto(self)->float:
+        return self.calcularSueldoBruto() - self.calcularRetenciones()
+    
+    def calcularSueldoBruto(self)->float:
+        raise LookupError
+    
+    def calcularRetenciones(self)->float:
+        raise LookupError
 
 
 class Empresa:
-    def __init__(self, cuit: str, nombre: str, empleados: list):
+    def __init__(self, cuit: str, nombre: str):
         self.cuit: str = cuit
         self.nombre: str = nombre
-        self.empleados: list[Empleado] = empleados
+        self.empleados: list[Empleado] = []
         self.recibosDeHaberes: list = []
     
     def calcularTotalSueldos(self)->float :
@@ -36,29 +56,10 @@ class Empresa:
         for empleado in self.empleados:
             self.recibosDeHaberes.append(ReciboDeHaberes(empleado, date.today))
             
+    def contratarEmpleado(self, empleado: Empleado):
+        self.empleados.append(empleado)
+            
         
-        
-class Empleado(ABC):
-    def __init__(self, nombre: str, dirección: str, estadoCivil: str, fechaDeNacimiento: date, sueldoBásico: float):
-        self.nombre: str = nombre
-        self.dirección: str = dirección
-        self.estadoCivil: str = estadoCivil
-        self.fechaDeNacimiento: date = fechaDeNacimiento
-        self.sueldoBásico: float = sueldoBásico
-        
-    def calcularEdad(self)->int:
-        return self.fechaDeNacimiento.year - date.today().year
-    
-    def calcularSueldoNeto(self)->float:
-        sueldoBruto = self.calcularSueldoBruto()
-        return self.calcularSueldoBruto() - self.calcularRetenciones(sueldoBruto)
-    
-    def calcularSueldoBruto(self)->float:
-        raise LookupError
-    
-    def calcularRetenciones(self, sueldoBruto: float)->float:
-        raise LookupError
-    
     
     
 class EmpleadoTemporario(Empleado):
@@ -71,7 +72,8 @@ class EmpleadoTemporario(Empleado):
         horasExtra = 40 * self.cantidadDeHorasExtra
         return self.sueldoBásico + horasExtra
     
-    def calcularRetenciones(self, sueldoBruto: float)->float:
+    def calcularRetenciones(self)->float:
+        sueldoBruto = self.calcularSueldoBruto()
         asignaciónPorEdad = 25 if self.calcularEdad() > 50 else 0
         obraSocial = sueldoBruto * 0.1 + asignaciónPorEdad
         aportesJubilatorios = sueldoBruto * 0.1 + 5 * self.cantidadDeHorasExtra
@@ -87,7 +89,7 @@ class EmpleadoContratado(Empleado):
     def calcularSueldoBruto(self)->float:
         return self.sueldoBásico
     
-    def calcularRetenciones(self, sueldoBruto: float)->float:
+    def calcularRetenciones(self)->float:
         gastosAdministrativosContractuales = 50
         
         return gastosAdministrativosContractuales
@@ -110,7 +112,8 @@ class EmpleadoPermanente(Empleado):
         return self.sueldoBásico + salarioFamiliar
         
     
-    def calcularRetenciones(self, sueldoBruto: float)->float:
+    def calcularRetenciones(self)->float:
+        sueldoBruto = self.calcularSueldoBruto()
         aportesJubilatorios = sueldoBruto * 0.15
         obraSocial = sueldoBruto * 0.1 + 20 * self.cantidadHijos
         
@@ -125,9 +128,8 @@ class ReciboDeHaberes:
         self.sueldoBruto: float = empleado.calcularSueldoBruto()
         self.sueldoNeto: float = empleado.calcularSueldoNeto()
         self.desgloce: str = f'''
-        {self.sueldoBruto}
-        {empleado.calcularRetenciones(self.sueldoBruto)}
-        {self.sueldoNeto}
+        Sueldo bruto: {self.sueldoBruto}
+        Retenciones: {empleado.calcularRetenciones()}
         '''      
         
     def __repr__(self) -> str:
@@ -139,7 +141,7 @@ class ReciboDeHaberes:
 # juan = EmpleadoPermanente(nombre="Juan", antigüedad=5, dirección="Dean Funes 850", estadoCivil= "Soltero", fechaDeNacimiento=date(1980, 5, 6), sueldoBásico=2500, cantidadHijos=0)
 # pepe = EmpleadoTemporario(cantidadDeHorasExtra=2, fechaDeFin=date(2025, 1, 1), nombre="pepe", dirección="Lavalle 500", estadoCivil="Soltero", fechaDeNacimiento=date(1998, 12, 3), sueldoBásico=1000)
 
-# empresa = Empresa(cuit="20232444", nombre="San Juan inc.", empleados=[pepe, juan, roberto])
+# empresa = Empresa(cuit="20232444", nombre="San Juan inc.")
 
 
 # print("SUELDOS TOTALES ------------------------------------------")
@@ -157,25 +159,57 @@ class ReciboDeHaberes:
 # print("----------------------------------------------------------")
 
 
+def test02():
+    roberto = EmpleadoPermanente(nombre="Roberto", antigüedad=2, dirección="Dean Funes 125", estadoCivil= "Casado", fechaDeNacimiento=date(2000, 1, 1), sueldoBásico=2000, cantidadHijos=2)
+    pepe = EmpleadoTemporario(cantidadDeHorasExtra=2, fechaDeFin=date(2025, 1, 1), nombre="pepe", dirección="Lavalle 500", estadoCivil="Soltero", fechaDeNacimiento=date(1995, 12, 3), sueldoBásico=1000)
+    agustín = EmpleadoTemporario(cantidadDeHorasExtra=5, fechaDeFin=date(2026, 5, 1), nombre="agustín", dirección="San Martín 506", estadoCivil="Casado", fechaDeNacimiento=date(2000, 12, 3), sueldoBásico=1000)
+    miguel = EmpleadoContratado(númeroDeContrato="0001", medioDePago="Tarjeta", nombre="miguel", dirección="Belgrano 5000", estadoCivil="Soltero", fechaDeNacimiento=date(1998, 12, 1), sueldoBásico=1000)
+    
+    empleados = [roberto, pepe, agustín, miguel]
+    
+    empresa = Empresa(cuit="20232466", nombre="Corpus inc.")
 
-roberto = EmpleadoPermanente(nombre="Roberto", antigüedad=2, dirección="Dean Funes 125", estadoCivil= "Casado", fechaDeNacimiento=date(2000, 1, 1), sueldoBásico=2000, cantidadHijos=2)
-pepe = EmpleadoTemporario(cantidadDeHorasExtra=2, fechaDeFin=date(2025, 1, 1), nombre="pepe", dirección="Lavalle 500", estadoCivil="Soltero", fechaDeNacimiento=date(1995, 12, 3), sueldoBásico=1000)
-agustín = EmpleadoTemporario(cantidadDeHorasExtra=5, fechaDeFin=date(2026, 5, 1), nombre="agustín", dirección="San Martín 506", estadoCivil="Casado", fechaDeNacimiento=date(2000, 12, 3), sueldoBásico=1000)
-miguel = EmpleadoContratado(númeroDeContrato="0001", medioDePago="Tarjeta", nombre="miguel", dirección="Belgrano 5000", estadoCivil="Soltero", fechaDeNacimiento=date(1998, 12, 1), sueldoBásico=1000)
+    for empleado in empleados:
+        empresa.contratarEmpleado(empleado)
+        
+    print("")    
+    print("SUELDOS TOTALES ------------------------------------------")
+    print(f"          {empresa.calcularTotalSueldos()}")
+    print("----------------------------------------------------------")
 
-empresa = Empresa(cuit="20232466", nombre="Corpus inc.", empleados=[pepe, agustín, roberto, miguel])
+    empresa.realizarLiquidaciónDeSueldos()
+
+    print("LIQUIDACIÓN -------------------------------------------------")
+    print("")
+    print(empresa.recibosDeHaberes)
+    print("")
+    print("----------------------------------------------------------")
+    
+def test01():
+    roberto = EmpleadoPermanente(nombre="Roberto", antigüedad=2, dirección="Dean Funes 125", estadoCivil= "Casado", fechaDeNacimiento=date(2000, 1, 1), sueldoBásico=2000, cantidadHijos=2)
+    juan = EmpleadoPermanente(nombre="Juan", antigüedad=5, dirección="Dean Funes 850", estadoCivil= "Soltero", fechaDeNacimiento=date(1980, 5, 6), sueldoBásico=2500, cantidadHijos=0)
+    pepe = EmpleadoTemporario(cantidadDeHorasExtra=2, fechaDeFin=date(2025, 1, 1), nombre="pepe", dirección="Lavalle 500", estadoCivil="Soltero", fechaDeNacimiento=date(1998, 12, 3), sueldoBásico=1000)
+    rich = EmpleadoContratado(númeroDeContrato="0000", medioDePago="Tarjeta", nombre="rich", dirección="Belgrano 5000", estadoCivil="Casado", fechaDeNacimiento=date(1995, 12, 1), sueldoBásico=15000)
+
+    empresa = Empresa(cuit="20232444", nombre="San Juan inc.")
+    
+    empleados = [roberto, pepe, juan, rich]
+
+    for empleado in empleados:
+        empresa.contratarEmpleado(empleado)
+        
+    print("")    
+    print("SUELDOS TOTALES ------------------------------------------")
+    print(f"          {empresa.calcularTotalSueldos()}")
+    print("----------------------------------------------------------")
+
+    empresa.realizarLiquidaciónDeSueldos()
+
+    print("LIQUIDACIÓN -------------------------------------------------")
+    print("")
+    print(empresa.recibosDeHaberes)
+    print("")
+    print("----------------------------------------------------------")
 
 
-print("SUELDOS TOTALES ------------------------------------------")
-print("")
-print(empresa.calcularTotalSueldos())
-print("")
-print("----------------------------------------------------------")
-
-empresa.realizarLiquidaciónDeSueldos()
-
-print("LIQUIDACIÓN -------------------------------------------------")
-print("")
-print(empresa.recibosDeHaberes)
-print("")
-print("----------------------------------------------------------")
+test01()
